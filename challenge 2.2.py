76a5b4696929b2ec40d3f83f5200bce637c44b62
Menakabalab <148324400+Menakabalab@users.noreply.github.com>
2023-10-18 14:22:44 +0530

    Add files via upload

---
+++ challenge 2.2.py
@@ -0,0 +1,37 @@
+#2.2 Implement a class called Player that represents a cricket player. The Player class should have a method called play() which prints "The player is playing cricket. Derive two classes, Batsman and Bowler, from the Player class. Override the play() method in each derived class to print "The batsman is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the Batsman and Bowler classes and call the play() method for each object.#2.2 implement a class called player that represents a cricket player. the player class should have a method called play() which prints "the player is playing cricket. derive two classes, batsman and bowler, from the player class. override the play() method in each derived class to print "the batsman is batting" and "the bowler is bowling", respectively. write a program to create objects of both the batsman and bowler classes and call the play() method for each object.
+
+# define the player class
+
+class player:
+
+    def play(self):
+
+        print("the player is playing cricket.")
+
+# define the batsman class, derived from player
+
+class batsman(player):
+
+    def play(self):
+
+        print("the batsman is batting.")
+
+# define the bowler class, derived from player
+
+class bowler(player):
+
+    def play(self):
+
+        print("the bowler is bowling.")
+
+# create objects of batsman and bowler classes
+
+batsman = batsman()
+
+bowler = bowler()
+
+# call the play() method for each object
+
+batsman.play()
+
+bowler.play()
